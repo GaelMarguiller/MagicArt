8c9b324127c621fb849c97cfd5dd7a0d9bf5580f
GMarguiller <gael.marguiller@supinternet.fr>
2021-11-28 11:58:42 +0100

    Fix bug

--- pages/set/[id].tsx
+++ pages/set/[id].tsx
@@ -4,39 +4,41 @@ import {SetsInterface} from '../../lib/interface/setsInterface';
 import {ROOT_URL} from '../../lib/constant/apiUrl';
 import {BlockCard} from '../../components/blockCard/blockCard';
 
-export default function SetCard({ allSetsData }) {
+export default function SetCard({allSetsData}) {
     return (
         <Layout>
-            <section className='max-w-7xl grid grid-cols-1 text-center md:grid-cols-4 m-0 m-auto'>
-                { allSetsData.map((setData) => (
-                    <BlockCard
-                        key={setData.id}
-                        name={ setData.name }
-                        artist={ setData.artist }
-                        colors={ setData.colors }
-                        imgArtCrop={ setData.imgArtCrop }
-                    />
-                    )
-                )}
+            <section className="max-w-7xl grid grid-cols-1 text-center md:grid-cols-4 m-0 m-auto">
+                {!allSetsData
+                    ? <p>Card is coming</p>
+                    : allSetsData.map((setData) => (
+                            <BlockCard
+                                key={setData.id}
+                                name={setData.name}
+                                artist={setData.artist}
+                                colors={setData.colors}
+                                imgArtCrop={setData.imgArtCrop}
+                            />
+                        )
+                    )}
             </section>
         </Layout>
     );
 }
 
 export async function getStaticPaths() {
-    const paths = await getAllSetIds(ROOT_URL)
+    const paths = await getAllSetIds(ROOT_URL);
     return {
         paths,
         fallback: false
-    }
+    };
 }
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({params}) {
     const allSetsData: SetsInterface[] = await getAllCardsSet(params.id);
 
     return {
         props: {
             allSetsData
         }
-    }
+    };
 }
